64ff39a3c1a2fcf79f799bd9d214fb81c0e39bb0
salesforcemarvin <mlacuna@salesforce.com>
2024-11-22 12:17:26 +0800

    Update customActivity.js

--- public/customActivity.js
+++ public/customActivity.js
@@ -17,21 +17,24 @@ define(["postmonger"], function (Postmonger) {
 
   function initialize(data) {
 
-    document.getElementById("configuration").value = JSON.stringify(
-      data,
-      null,
-      2
-    );
+    // document.getElementById("configuration").value = JSON.stringify(
+    //   data,
+    //   null,
+    //   2
+    // );
+    document.getElementById("configuration").value = data;
+
   }
 
   //Save Sequence
   function onClickedNext() {
-    var configuration = JSON.parse(
-      document.getElementById("configuration").value
-    );
+    // var configuration = JSON.parse(
+    //   document.getElementById("configuration").value
+    // );
+    var configuration = document.getElementById("configuration").value;
     connection.trigger("updateActivity", configuration);
 
-    //var configuration = document.getElementById("configuration").value;
+    
     //save(configuration);
   }
 
