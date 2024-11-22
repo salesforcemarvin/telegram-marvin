08f3f9b17c7155cb8ab2670bbbbbbf8548976c09
salesforcemarvin <mlacuna@salesforce.com>
2024-11-22 12:10:20 +0800

    Update customActivity.js

--- public/customActivity.js
+++ public/customActivity.js
@@ -9,7 +9,6 @@ define(["postmonger"], function (Postmonger) {
 
   connection.on("initActivity", initialize);
   connection.on("clickedNext", onClickedNext);
-  connection.on("gotoStep", onClickedNext);
 
   function onRender() {
     // JB will respond the first time 'ready' is called with 'initActivity'
@@ -17,35 +16,6 @@ define(["postmonger"], function (Postmonger) {
   }
 
   function initialize(data) {
-
-    // if (data) {
-    //   payload = data;
-    // }
-
-    //Checking if inArguments Exist
-      // var message;
-      // var hasInArguments = Boolean(
-      //   payload["arguments"] &&
-      //     payload["arguments"].execute &&
-      //     payload["arguments"].execute.inArguments &&
-      //     payload["arguments"].execute.inArguments.length > 0
-      // );
-
-    // //if arguement is empty then {}
-      // var inArguments = hasInArguments
-      // ? payload["arguments"].execute.inArguments
-      // : {};
-
-    //Iterates through the key-value pairs of the inArgument object.
-    //Checks if the current key is "message".
-      //If the key matches, the value (val) is assigned to the message variable.
-      // $.each(inArguments, function (index, inArgument) {
-      //   $.each(inArgument, function (key, val) {
-      //     if (key === "message") {
-      //       message = val;
-      //     }
-      //   });
-      // });
 
     document.getElementById("configuration").value = JSON.stringify(
       data,
@@ -59,10 +29,10 @@ define(["postmonger"], function (Postmonger) {
     var configuration = JSON.parse(
       document.getElementById("configuration").value
     );
-    
-    //var configuration = document.getElementById("configuration").value;
+    connection.trigger("updateActivity", configuration);
 
-    save(configuration);
+    //var configuration = document.getElementById("configuration").value;
+    //save(configuration);
   }
 
   function save(tdata) {
@@ -74,7 +44,7 @@ define(["postmonger"], function (Postmonger) {
       // may be overridden as desired.
       payload.name = tdata; //text message to send to telegram
 
-      payload["arguments"].execute.inArguments = [{ message: tdata }];
+      //payload["arguments"].execute.inArguments = [{ message: tdata }];
   
       payload["metaData"].isConfigured = true;
 
