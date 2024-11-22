df4e3f9a2558403c765a64d401dfc270668dc73d
salesforcemarvin <mlacuna@salesforce.com>
2024-11-23 02:36:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,21 +6,17 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 app.post("/save", (req, res) => {
-  console.log('Saving custom activity configuration');
   // Handle save request
   res.sendStatus(200);
 });
 app.post("/publish", (req, res) => {
-  console.log('Publishing custom activity');
   // Handle publish request
   res.sendStatus(200);
 });
 app.post("/validate", (req, res) => {
-  console.log('Validating custom activity');
   // Handle validate request
   res.sendStatus(200);
 });
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
