8651d31105da1544d725bb9201e9b6df5bb6778d
salesforcemarvin <mlacuna@salesforce.com>
2024-11-23 02:23:08 +0800

    dfgh

--- index.js
+++ index.js
@@ -6,16 +6,24 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 app.post("/save", (req, res) => {
+  console.log("Saving custom activity configuration");
   // Handle save request
   res.sendStatus(200);
 });
 app.post("/publish", (req, res) => {
+  console.log("Publishing custom activity");
   // Handle publish request
   res.sendStatus(200);
 });
 app.post("/validate", (req, res) => {
+  console.log("Validating custom activity");
   // Handle validate request
   res.sendStatus(200);
+});
+app.post("/stop", (req, res) => {
+  console.log("Stopping custom activity");
+  // Endpoint to handle the stop action
+  res.status(200).send({ status: "success" });
 });
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
