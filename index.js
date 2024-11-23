7fcc32087183deccc00c5487e087ef2c2d75eec8
salesforcemarvin <mlacuna@salesforce.com>
2024-11-23 21:28:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -76,8 +76,8 @@ app.post("/execute", (req, res) => {
 
       const response = axios.get(
         "https://api.telegram.org/bot7622096585:AAHe3Tdc4zsc9-9hKvY0C5briAUo4QSIUWs/sendMessage?chat_id=@vcbsalesforce&text=" +
-          "333 " +
-          inArguments.customField
+          "333 " 
+          //+ inArguments.customField
           
       );
 
