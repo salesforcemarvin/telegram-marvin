9a83fd26b5e7dbfb62a94172cc4ef9729a4ce3fa
salesforcemarvin <mlacuna@salesforce.com>
2024-11-23 14:02:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ app.post("/validate", (req, res) => {
 app.post("/execute", (req, res) => {
   // Endpoint to handle the execution of the custom activity
   try {
-    // const inArguments = req.body.inArguments[0];
+    const inArguments = req.body.inArguments[0];
     // const contactKey = inArguments.contactKey;
-    // const emailAddress = inArguments.emailAddress;
+    const emailAddress = inArguments.emailAddress;
     // const firstName = inArguments.firstName;
     // const lastName = inArguments.lastName;
     // const customField = inArguments.customField;
@@ -39,7 +39,7 @@ app.post("/execute", (req, res) => {
       //   data: req.body,
       // });
 
-      const response = axios.get("https://api.telegram.org/bot7622096585:AAHe3Tdc4zsc9-9hKvY0C5briAUo4QSIUWs/sendMessage?chat_id=@vcbsalesforce&text=marvin!");
+      const response = axios.get("https://api.telegram.org/bot7622096585:AAHe3Tdc4zsc9-9hKvY0C5briAUo4QSIUWs/sendMessage?chat_id=@vcbsalesforce&text=" + emailAddress);
 
       res.send(response.data);
     } catch (error) {
