be90091ff14a962b15fbc907312fe010523ffd5a
salesforcemarvin <mlacuna@salesforce.com>
2024-11-23 21:03:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,15 +33,15 @@ app.post("/execute", (req, res) => {
   //   },
   // };
 
-  const fs = require("fs");
-  fs.readFile(".public/config.json", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading the file:", err);
-      return;
-    }
-    const config = JSON.parse(data);
-    console.log(config);
-  });
+  // const fs = require("fs");
+  // fs.readFile(".public/config.json", "utf8", (err, data) => {
+  //   if (err) {
+  //     console.error("Error reading the file:", err);
+  //     return;
+  //   }
+  //   const config = JSON.parse(data);
+  //   console.log(config);
+  // });
 
 
 
@@ -77,8 +77,8 @@ app.post("/execute", (req, res) => {
       const response = axios.get(
         "https://api.telegram.org/bot7622096585:AAHe3Tdc4zsc9-9hKvY0C5briAUo4QSIUWs/sendMessage?chat_id=@vcbsalesforce&text=" +
           "333 " +
-          //inArguments.customField
-          config
+          inArguments.customField
+          
       );
 
       res.send(response.data);
