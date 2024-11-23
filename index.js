c8dc96f252752de978226b2942869dc2b4f33272
salesforcemarvin <mlacuna@salesforce.com>
2024-11-23 14:58:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,8 @@ app.post("/execute", (req, res) => {
   
   try {
 
-    const inArguments = req["arguments"].execute.inArguments[0];
+    //const inArguments = req["arguments"].execute.inArguments[0];
+    var inArguments = req["arguments"].execute.inArguments[0];
     
     // const contactKey = inArguments.contactKey;
     //const emailAddress = inArguments.emailAddress;
